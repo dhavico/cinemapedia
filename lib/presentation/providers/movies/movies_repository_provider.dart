9e2dcf74353c0def5ccd65023922d20094717469
David Ortega <dhavico@hotmail.com>
2023-09-06 16:31:31 -0500

    avance sección 15

--- lib/presentation/providers/movies/movies_repository_provider.dart
+++ lib/presentation/providers/movies/movies_repository_provider.dart
@@ -1,6 +1,6 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:cinemapedia/infrastructure/datasources/moviedb_datasource_impl.dart';
 import 'package:cinemapedia/infrastructure/repositories/movie_repository_impl.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 //Este repositorio es inmutable(solo lectura)
 final movieRepositoryProvider = Provider((ref) {
