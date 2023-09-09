b763cb28523aac04a719ad36ae5fb7d092657f12
David Ortega <dhavico@hotmail.com>
2023-09-08 23:19:48 -0500

    finalizando la sección 17

---
+++ lib/presentation/widgets/movies/movie_poster_link.dart
@@ -0,0 +1,35 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class MoviePosterLink extends StatelessWidget {
+  final Movie movie;
+  const MoviePosterLink({super.key, required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeInUp(
+      child: GestureDetector(
+        onTap: () => context.push('/home/0/movie/${movie.id}'),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Stack(
+            children: [
+              Image.network(movie.posterPath),
+              Center(
+                  child: Container(
+                color: Colors.white,
+                child: Text(
+                  movie.isarId.toString(),
+                  style: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.w800),
+                ),
+              ))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
